a7894123a877eb286e43474a5ec9c5ca38dd37e0
Nicolas Luck <nicolas@lucksus.eu>
2019-06-25 21:39:05 +0200

    Adjust middleware to conductorMaps

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,6 @@
 
+const _ = require('lodash')
+
 import {ScenarioFnCustom} from './types'
 
 export const compose = (...ms) => (f: ScenarioFnCustom): ScenarioFnCustom => ms.reduce((g, m) => m(g), f)
@@ -6,28 +8,32 @@ export const compose = (...ms) => (f: ScenarioFnCustom): ScenarioFnCustom => ms.
 /**
  * Middleware to retrofit each instance with a `callSync` method
  */
-export const callSyncMiddleware = f => (s, ins) => {
+export const callSyncMiddleware = f => (s, conductors) => {
   // callSync "polyfill"
-  Object.values(ins).forEach((i: any) => {
-    i.callSync = async (...args) => {
+  _.chain(conductors)
+    .values()
+    .forEach(c => c.values().forEach(i => {
+      i.callSync = async (...args) => {
         const ret = await i.call(...args)
         await s.consistent()
         return ret
-    }
-  })
-  return f(s, ins)
+      }
+    }))
+  return f(s, conductors)
 }
 
 /**
  * Middleware to retrofit each instance with an `agentId` member,
  * equivalent to the `agentAddress`
  */
-export const agentIdMiddleware = f => (s, ins) => {
+export const agentIdMiddleware = f => (s, conductors) => {
     // agentId "polyfill"
-  Object.values(ins).forEach((i: any) => {
-    i.agentId = i.agentAddress
-  })
-  return f(s, ins)
+  _.chain(conductors)
+    .values()
+    .forEach(c => c.values().forEach(i => {
+      i.agentId = i.agentAddress
+    }))
+  return f(s, conductors)
 }
 
 export const backwardCompatibilityMiddleware = compose(callSyncMiddleware, agentIdMiddleware)
