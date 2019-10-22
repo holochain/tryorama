7ae8046292a53a47af704370edfc89563a70d979
Michael Dougherty <maackle.d@gmail.com>
2019-10-22 08:57:58 -0700

    Expose types (is this necessary?)

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import { dna, bridge, dpki, genConfig } from './config'
 
 export const Config = { dna, bridge, dpki, genConfig }
 export { Orchestrator } from './orchestrator'
+export { Player } from './player'
+export { Instance } from './instance'
 export * from './middleware'
+export * from './types'
