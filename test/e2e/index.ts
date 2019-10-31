48c6e2c4120a19e75556af915af4467bfa3de754
Michael Dougherty <maackle.d@gmail.com>
2019-10-31 10:56:01 -0700

    Turn on all e2e tests

--- test/e2e/index.ts
+++ test/e2e/index.ts
@@ -2,29 +2,24 @@
 import { Orchestrator } from '../../src'
 import { runSeries, combine, singleConductor, machinePerPlayer, localOnly } from '../../src/middleware'
 
-const network = {
-  type: 'sim1h',
-  dynamo_url: 'http://localhost:8000',
-}
-
 const localOrchestrator = () => new Orchestrator({
   middleware: combine(runSeries, localOnly),
   reporter: true,
-  // globalConfig is specified explicitly in testConfig in this case
+  // globalConfig is specified explicitly in common::testConfig in this case
 })
 
 const mrmmOrchestrator = () => new Orchestrator({
   middleware: combine(runSeries, machinePerPlayer('MOCK')),
   reporter: true,
-  // globalConfig is specified explicitly in testConfig in this case
+  // globalConfig is specified explicitly in common::testConfig in this case
 })
 
 const singleConductorOrchestrator = () => new Orchestrator({
   middleware: combine(runSeries, singleConductor, localOnly),
   reporter: true,
-  // globalConfig is specified explicitly in testConfig in this case
+  // globalConfig is specified explicitly in common::testConfig in this case
 })
 
-// require('./test-always-on')(localOrchestrator)
-// require('./test-always-on')(singleConductorOrchestrator)
+require('./test-always-on')(localOrchestrator)
+require('./test-always-on')(singleConductorOrchestrator)
 require('./test-always-on')(mrmmOrchestrator)
