33833e9c810beee3f1ffbba55b4c914428c19f46
Michael Dougherty <maackle.d@gmail.com>
2020-03-18 11:39:35 -0700

    Update dna to be fetched

--- test/fetch-dna.ts
+++ test/fetch-dna.ts
@@ -3,7 +3,7 @@ import { mkdirIdempotent } from "../src/config";
 
 mkdirIdempotent('dna').then(() => {
   downloadFile({
-    url: 'https://github.com/holochain/passthrough-dna/releases/download/v0.0.5/passthrough-dna.dna.json', 
+    url: 'https://github.com/holochain/passthrough-dna/releases/download/v0.0.7-rc1/passthrough-dna.dna.json',
     path: './dna/passthrough-dna.dna.json'
   })
 })
