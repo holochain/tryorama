b9dabdf55b8cc059cea6b9d47da10f50ea78c378
Eric Harris-Braun <eric@harris-braun.com>
2020-11-14 11:05:46 -0500

    more wip e2e tests

--- test/e2e/test-dynamic-on.ts
+++ test/e2e/test-dynamic-on.ts
@@ -14,20 +14,16 @@ module.exports = (testOrchestrator, testConfig) => {
     orchestrator.registerScenario('attempted call with stopped conductor', async s => {
       const [ alice ] = await s.players([C.players.alice], false)
       await alice.startup()
-      const [alice_happ] = await alice.installAgentsHapps(C.initialization)
-
+      const [[alice_happ]] = await alice.installAgentsHapps(C.initialization)
+      const link_cell = alice_happ.cells[0]
       await t.doesNotReject(
-        alice_happ.cells[0].call('main', 'commit_entry', {
-          content: 'content'
-        })
+        link_cell.call('link', 'create_link', undefined)
       )
 
       await alice.shutdown()
 
       await t.rejects(
-        alice_happ.cells[0].call('main', 'commit_entry', {
-          content: 'content'
-        })
+        link_cell.call('link', 'create_link', undefined)
         /* no conductor is running.*/
       )
     })
@@ -35,12 +31,12 @@ module.exports = (testOrchestrator, testConfig) => {
     orchestrator.registerScenario('start-stop-start', async s => {
       const [ alice ] = await s.players([C.players.alice], false)
       await alice.startup()
-      const [alice_happ] = await alice.installAgentsHapps(C.initialization)
+      const [[alice_happ]] = await alice.installAgentsHapps(C.initialization)
       await alice.shutdown()
       await alice.startup()
-      const agentAddress = await alice_happ.cells[0].call('main', 'commit_entry', {
-        content: 'content'
-      })
+      const link_cell = alice_happ.cells[0]
+      const agentAddress = await link_cell.call('link', 'create_link', undefined)
+
       t.equal(agentAddress.Ok.length, 46)
     })
 
@@ -49,54 +45,41 @@ module.exports = (testOrchestrator, testConfig) => {
     t.equal(stats.successes, 2)
   })
 
-  test.skip('late joiners', async t => {
+  test('late joiners', async t => {
     const C = testConfig()
     const orchestrator = testOrchestrator()
     orchestrator.registerScenario('attempted call with unspawned conductor', async s => {
       const [ alice ] = await s.players([C.players.alice])
-      const [alice_happ] = await alice.installAgentsHapps(C.initialization)
+      const [[alice_happ]] = await alice.installAgentsHapps(C.initialization)
 
-      const [alice_cell] = alice_happ.cells[0]
+      const alice_cell = alice_happ.cells[0]
 
-      const commit1 = await alice_cell.call('main', 'commit_entry', {
-        content: 'content1'
-      })
-      const commit2 = await alice_cell.call('main', 'commit_entry', {
-        content: 'content2'
-      })
-      const hash1 = commit1.Ok
-      const hash2 = commit2.Ok
+      const linkResult = await alice_cell.call('link', 'create_link', undefined)
 
-      const linkResult = await alice_cell.call('main', 'link_entries', {
-        base: hash1,
-        target: hash2,
-      })
       const linkHash = linkResult.Ok
-      await s.consistency()
+      //await s.consistency()
 
       // bob and carol join later
       const [ bob, carol ] = await s.players([C.players.bob, C.players.carol ])
-      const [bob_happ] = await bob.installAgentsHapps(C.initialization)
-      const [carol_happ] = await carol.installAgentsHapps(C.initialization)
+      const [[bob_happ]] = await bob.installAgentsHapps(C.initialization)
+      const [[carol_happ]] = await carol.installAgentsHapps(C.initialization)
 
       // after the consistency waiting inherent in auto-spawning the new players, their state dumps
       // should immediately show that they are holding alice's entries
-      const bobDump = await bob.stateDump('app')
-      const carolDump = await bob.stateDump('app')
+      // FIXME stateDump
+//      const bobDump = await bob.stateDump('app')
+//      const carolDump = await bob.stateDump('app')
 
-      t.ok(hash1 in bobDump.held_aspects)
+      // FIXME
+/*      t.ok(hash1 in bobDump.held_aspects)
       t.ok(hash2 in bobDump.held_aspects)
       t.ok(linkHash in bobDump.held_aspects)
       t.ok(hash1 in carolDump.held_aspects)
       t.ok(hash2 in carolDump.held_aspects)
       t.ok(linkHash in carolDump.held_aspects)
-
-      const bobLinks = await bob_happ.cells[0].call('main', 'get_links', {
-        base: hash1
-      })
-      const carolLinks = await carol_happ.cells[0].call('main', 'get_links', {
-        base: hash1
-      })
+*/
+      const bobLinks = await bob_happ.cells[0].call('link', 'get_links', undefined)
+      const carolLinks = await carol_happ.cells[0].call('link', 'get_links', undefined)
 
       t.equal(bobLinks.Ok.links.length, 1)
       t.equal(carolLinks.Ok.links.length, 1)
