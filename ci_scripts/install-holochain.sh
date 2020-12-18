c873e578d3d8c91a4c16f4991213bfe750885519
Eric Harris-Braun <eric@harris-braun.com>
2020-12-17 20:34:54 -0500

    bump holochain

--- ci_scripts/install-holochain.sh
+++ ci_scripts/install-holochain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REV=cf4e72416e5afbf29b86d66a3d47ab2f9f6a65d2
+REV=60a906212c17ee067b31511e6b2957746d86297b
 
 cargo install --force holochain \
   --git https://github.com/holochain/holochain.git \
