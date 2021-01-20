a05c3a08501c349c0331b56908e219302e5a0e70
timotree3 <timorcb@gmail.com>
2021-01-20 13:08:45 -0500

    Fix CI

--- ci_scripts/run-test.sh
+++ ci_scripts/run-test.sh
@@ -2,7 +2,7 @@
 echo `pwd`
 cd test/e2e/fixture/zomes/test
 cargo build --release --target wasm32-unknown-unknown --target-dir ../../../../../target
-cp target/wasm32-unknown-unknown/release/test_wasm.wasm ../../../test.dna.workdir/test.wasm
+cp ../../../../../target/wasm32-unknown-unknown/release/test_wasm.wasm ../../../test.dna.workdir/test.wasm
 cd ../../..
 dna-util -c test.dna.workdir
 cd ../..
