8fbe7ba62ed91bec71736ea5423595f4e42566bf
timotree3 <timorcb@gmail.com>
2021-01-26 09:41:27 -0500

    Improve ci script

--- ci_scripts/install-holochain.sh
+++ ci_scripts/install-holochain.sh
@@ -3,6 +3,8 @@
 REV=ea026def01d1573d5e0edfb7f4e3e9453f88c43e
 LAIR_REV=be5868e6dcbe99c795a101c0e27ba6ed5edd557d
 
+export CARGO_TARGET_DIR="$PWD/target"
+echo $CARGO_TARGET_DIR
 cargo install --force holochain \
   --git https://github.com/holochain/holochain.git \
   --rev $REV
