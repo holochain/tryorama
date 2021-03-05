c48031434805efb3e32a213abff6b2f4bf51959f
jetttech <lisa.jetton@holo.host>
2021-03-04 22:27:40 -0600

    add optional app port to CommonConfig

--- test/e2e/test-always-on.ts
+++ test/e2e/test-always-on.ts
@@ -2,6 +2,8 @@ import * as tape from 'tape'
 import test from 'tape-promise/tape'
 
 import { ScenarioApi } from '../../src/api';
+import { Config } from '../../src'
+import * as T from '../../src/types'
 
 export default (testOrchestrator, testConfig, playersFn = (s, ...args) => s.players(...args)) => {
 
@@ -29,7 +31,16 @@ export default (testOrchestrator, testConfig, playersFn = (s, ...args) => s.play
     const [conductorConfig, installApps] = testConfig()
     const orchestrator = await testOrchestrator()
     orchestrator.registerScenario('simple zome call', async (s: ScenarioApi) => {
-      const [alice] = await playersFn(s, [conductorConfig])
+      const seed: T.ConfigSeed = Config.gen({
+        network: {
+          transport_pool: [{
+            type: T.TransportConfigType.Quic,
+          }],
+        },
+        appPort: 6000
+      }
+      )
+      const [alice] = await playersFn(s, [seed])
       const [[alice_happ]] = await alice.installAgentsHapps(installApps)
       const hash = await alice_happ.cells[0].call('test', 'create_link')
       t.equal(hash.length, 39, 'zome call succeeded')
