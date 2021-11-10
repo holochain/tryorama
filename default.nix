9b8fc0799db88d3f536bc8ca06f2e50484d786a1
Eric Harris-Braun <eric@harris-braun.com>
2021-11-10 14:25:18 -0500

    Updates default.nix to new format and conductor-api to 0.2.4

--- default.nix
+++ default.nix
@@ -1,32 +1,9 @@
 {
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/develop.tar.gz"; }
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/726694e2889a57a28b553541d2fabd8f1fbb52b2.tar.gz"; }
 }:
 
 let
-  holonix = import (holonixPath) {
-    include = {
-        # making this explicit even though it's the default
-        holochainBinaries = true;
-    };
-
-    holochainVersionId = "custom";
-
-    holochainVersion = {
-      rev = "holochain-0.0.114";
-      sha256 = "1r3fxmcnc6576cbq12vyzyjgdjf6754mfsivzplzmj47bwvx3hx1";
-      cargoSha256 = "15d8h3ivr8xdrccxgmpwn5sv4givhvfvvhihdhdgyv1893gpmzl3";
-      bins = {
-        holochain = "holochain";
-        hc = "hc";
-        kitsune-p2p-proxy = "kitsune_p2p/proxy";
-      };
-
-      lairKeystoreHashes = {
-        sha256 = "1zq8mpxcy8p7kbj4xl4qhp2hb0fjxakixhzcb4y1rnygc90q9v01";
-        cargoSha256 = "1ln0vx1blzjr4p9rqfhcl4b34blk6jiyziz2w5gh09wv2xbhyaa5";
-      };
-    };
-  };
+  holonix = import (holonixPath) { };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
