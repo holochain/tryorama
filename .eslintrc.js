959a31d9e19f73d7ad8b8abced1e6e614c760538
Jost Schulte <jost.schulte@protonmail.com>
2022-02-10 13:48:44 +0100

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
+// eslint-disable-next-line
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    rules: {
-        semi: ["error", "always"]
-    }
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
 };
