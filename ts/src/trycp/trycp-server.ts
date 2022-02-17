6cf1bf53827f7aa2b2aa7e1db0b457dfcca3b4ec
Jost Schulte <jost.schulte@protonmail.com>
2022-02-17 20:01:35 +0100

    test trycp server call: configure player

--- ts/src/trycp/trycp-server.ts
+++ ts/src/trycp/trycp-server.ts
@@ -3,7 +3,13 @@ import { makeLogger } from "../logger";
 
 const logger = makeLogger("TryCP server");
 
-export const PORT = 9000;
+export const TRYCP_SERVER_HOST = "0.0.0.0";
+export const TRYCP_SERVER_PORT = 9000;
+export const DEFAULT_PARTIAL_PLAYER_CONFIG = `signing_service_uri: ~
+  encryption_service_uri: ~
+  decryption_service_uri: ~
+  dpki: ~
+  network: ~`;
 
 export class TryCpServer {
   private serverProcess: ChildProcessWithoutNullStreams;
@@ -24,21 +30,27 @@ export class TryCpServer {
     );
   }
 
-  static async start(port = PORT) {
+  static async start(port = TRYCP_SERVER_PORT) {
     const tryCpServer = new TryCpServer(port);
 
+    tryCpServer.serverProcess.on("error", (err) =>
+      logger.error(`Error starting up local TryCP server ${err}`)
+    );
+
     tryCpServer.serverProcess.stderr.on("data", (data) => {
-      logger.info(`build process - ${data}`);
+      logger.debug(`build process - ${data}`);
     });
 
     const trycpPromise = new Promise<TryCpServer>((resolve) =>
       tryCpServer.serverProcess.stdout.on("data", (data) => {
-        const regex = new RegExp("Listening on 0.0.0.0:" + port);
+        const regex = new RegExp(
+          `Listening on ${TRYCP_SERVER_HOST}:${TRYCP_SERVER_PORT}`
+        );
         if (regex.test(data)) {
           logger.debug("started");
           resolve(tryCpServer);
         }
-        logger.info(data);
+        logger.debug(data);
       })
     );
     return trycpPromise;
@@ -46,7 +58,7 @@ export class TryCpServer {
 
   async stop() {
     // TODO send stop signal
-    this.serverProcess.on("exit", (code) => logger.info(`exit code ${code}`));
-    this.serverProcess.kill("SIGINT");
+    this.serverProcess.on("exit", (code) => logger.debug(`exit code ${code}`));
+    this.serverProcess.kill();
   }
 }
---
+++ ts/test/trycp-server.ts
@@ -0,0 +1,48 @@
+// import { AdminWebsocket } from "@holochain/client";
+import test from "tape";
+import {
+  TRYCP_SERVER_HOST,
+  TRYCP_SERVER_PORT,
+  TryCpServer,
+  DEFAULT_PARTIAL_PLAYER_CONFIG,
+} from "../src/trycp/trycp-server";
+import { TryCpClient } from "../src/trycp/trycp-client";
+
+const createTryCpClient = () =>
+  TryCpClient.create(`ws://${TRYCP_SERVER_HOST}:${TRYCP_SERVER_PORT}`);
+
+test("TryCP ping", async (assert) => {
+  const localTryCpServer = await TryCpServer.start();
+  const tryCpClient = await createTryCpClient();
+
+  const expected = "peng";
+  const actual = (await tryCpClient.ping(expected)).toString();
+
+  await tryCpClient.destroy();
+  await localTryCpServer.stop();
+
+  assert.equal(actual, expected);
+});
+
+test.only("TryCP call - configure player", async (assert) => {
+  const localTryCpServer = await TryCpServer.start();
+  const tryCpClient = await createTryCpClient();
+
+  const expectedRequestId = 1;
+  const actual = await tryCpClient.call({
+    id: expectedRequestId,
+    request: {
+      type: "configure_player",
+      id: "player1",
+      partial_config: "facke", //DEFAULT_PARTIAL_PLAYER_CONFIG,
+    },
+  });
+  await tryCpClient.destroy();
+  await localTryCpServer.stop();
+
+  console.log("response", actual);
+
+  assert.equal(actual.id, expectedRequestId);
+  assert.equal(actual.type, "response");
+  assert.equal(actual.response[0], null);
+});
