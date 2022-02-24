19dd5e55c24d3fffaea53c16bc8dbef83811eeab
Jost Schulte <jost.schulte@protonmail.com>
2022-02-24 22:17:46 +0100

    add TSdoc & lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,13 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc", "prettier"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
+  rules: {
+    "tsdoc/syntax": "warn"
+  }
 };
