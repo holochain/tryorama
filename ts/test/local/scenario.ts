d135a01969ffdb6c46226ae0faccd801cce35732
Jost Schulte <jost.schulte@protonmail.com>
2022-05-04 19:46:05 -0600

    test(local): conductor maintains state after restart

--- ts/test/local/scenario.ts
+++ ts/test/local/scenario.ts
@@ -29,3 +29,36 @@ test("Local Scenario - Create and read an entry, 2 conductors", async (t) => {
 
   await scenario.cleanUp();
 });
+
+test("Local Scenario - Conductor maintains data after shutdown and restart", async (t) => {
+  const scenario = new Scenario();
+  const [alice, bob] = await scenario.addPlayers([
+    [{ path: FIXTURE_DNA_URL.pathname }],
+    [{ path: FIXTURE_DNA_URL.pathname }],
+  ]);
+  const content = "Before shutdown";
+  const createEntryHash = await alice.cells[0].callZome<EntryHash>({
+    zome_name: "crud",
+    fn_name: "create",
+    payload: content,
+  });
+  await pause(500);
+  const readContent = await bob.cells[0].callZome<typeof content>({
+    zome_name: "crud",
+    fn_name: "read",
+    payload: createEntryHash,
+  });
+  t.equal(readContent, content);
+
+  await bob.conductor.shutDown();
+  t.rejects(bob.conductor.generateAgentPubKey);
+
+  await bob.conductor.startUp({});
+  const readContentAfterRestart = await bob.cells[0].callZome<typeof content>({
+    zome_name: "crud",
+    fn_name: "read",
+    payload: createEntryHash,
+  });
+  t.equal(readContentAfterRestart, content);
+  await scenario.cleanUp();
+});
