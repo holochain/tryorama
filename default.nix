8c86f402511ab7014f0cef01a2fcd9e816261870
Jost Schulte <jost.schulte@protonmail.com>
2023-01-23 15:42:59 -0600

    default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,21 @@
 let
   holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
+    holochainVersionId = "v0_1_0-beta-rc_4";
     include = {
+      holochainBinaries = true;
       node = false;
       happs = false;
       scaffolding = false;
       launcher = false;
     };
-    holochainVersionId = "v0_1_0-beta-rc_4";
   };
   nixpkgs = holonix.pkgs;
 in
 nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
-  packages = with nixpkgs; [ nodejs ];
+  packages = with nixpkgs; [
+    niv
+    nodejs
+  ];
 }
