b016d8e5962d3952cd1f2fd274de45cc774963d8
Jost Schulte <jost.schulte@protonmail.com>
2023-01-23 15:38:35 -0600

    remove more nix includes

--- default.nix
+++ default.nix
@@ -2,6 +2,9 @@ let
   holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
     include = {
+      node = false;
+      happs = false;
+      scaffolding = false;
       launcher = false;
     };
     holochainVersionId = "v0_1_0-beta-rc_4";
