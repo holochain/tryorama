e6fe0ec314792d68c2dfd6c5d05dd9b0697f1d1d
neonphog <neonphog@gmail.com>
2024-05-31 13:09:52 -0600

    add a rust trycp client crate

---
+++ crates/trycp_client/src/lib.rs
@@ -0,0 +1,138 @@
+#![deny(missing_docs)]
+//! Trycp client.
+
+use futures::{sink::SinkExt, stream::StreamExt};
+use std::collections::HashMap;
+use std::io::Result;
+use std::sync::Arc;
+use tokio_tungstenite::{
+    tungstenite::{client::IntoClientRequest, Message},
+    *,
+};
+pub use trycp_api::Request;
+use trycp_api::*;
+
+type WsCore = WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>;
+type WsSink = futures::stream::SplitSink<WsCore, Message>;
+type Ws = Arc<tokio::sync::Mutex<WsSink>>;
+
+/// Signal emitted from a conductor.
+pub struct Signal {
+    /// The app port from which this signal was emitted.
+    pub port: u16,
+
+    /// The content of the signal.
+    pub data: Vec<u8>,
+}
+
+/// Trycp client recv.
+pub struct SignalRecv(tokio::sync::mpsc::Receiver<Signal>);
+
+impl SignalRecv {
+    /// Receive.
+    pub async fn recv(&mut self) -> Option<Signal> {
+        self.0.recv().await
+    }
+}
+
+/// Trycp client.
+pub struct TrycpClient {
+    ws: Ws,
+    pend: Arc<
+        std::sync::Mutex<HashMap<u64, tokio::sync::oneshot::Sender<Result<serde_json::Value>>>>,
+    >,
+    recv_task: tokio::task::JoinHandle<()>,
+}
+
+impl Drop for TrycpClient {
+    fn drop(&mut self) {
+        let ws = self.ws.clone();
+        tokio::task::spawn(async move {
+            let _ = ws.lock().await.close().await;
+        });
+        self.recv_task.abort();
+    }
+}
+
+impl TrycpClient {
+    /// Connect to a remote trycp server.
+    pub async fn connect<R>(request: R) -> Result<(Self, SignalRecv)>
+    where
+        R: IntoClientRequest + Unpin,
+    {
+        let (w, _) = tokio_tungstenite::connect_async(request)
+            .await
+            .map_err(std::io::Error::other)?;
+
+        let (sink, mut stream) = w.split();
+
+        let map: HashMap<u64, tokio::sync::oneshot::Sender<Result<serde_json::Value>>> =
+            HashMap::new();
+        let pend = Arc::new(std::sync::Mutex::new(map));
+
+        let (recv_send, recv_recv) = tokio::sync::mpsc::channel(32);
+
+        let pend2 = pend.clone();
+        let recv_task = tokio::task::spawn(async move {
+            while let Some(Ok(msg)) = stream.next().await {
+                let msg = msg.into_data();
+                let msg: MessageToClient = rmp_serde::from_slice(&msg).unwrap();
+
+                match msg {
+                    MessageToClient::Signal { port, data } => {
+                        recv_send.send(Signal { port, data }).await.unwrap();
+                    }
+                    MessageToClient::Response { id, response } => {
+                        if let Some(resp) = pend2.lock().unwrap().remove(&id) {
+                            let _ = resp.send(response.map_err(std::io::Error::other));
+                        }
+                    }
+                }
+            }
+        });
+
+        let ws = Arc::new(tokio::sync::Mutex::new(sink));
+
+        Ok((
+            Self {
+                ws,
+                pend,
+                recv_task,
+            },
+            SignalRecv(recv_recv),
+        ))
+    }
+
+    /// Make a request of the trycp server.
+    pub async fn request(
+        &self,
+        request: Request,
+        timeout: std::time::Duration,
+    ) -> Result<serde_json::Value> {
+        static RID: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(1);
+        let mid = RID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+
+        let (s, r) = tokio::sync::oneshot::channel();
+
+        self.pend.lock().unwrap().insert(mid, s);
+
+        let pend = self.pend.clone();
+        tokio::task::spawn(async move {
+            tokio::time::sleep(timeout).await;
+            pend.lock().unwrap().remove(&mid);
+        });
+
+        let request = RequestWrapper { id: mid, request };
+
+        let request = rmp_serde::to_vec_named(&request).map_err(std::io::Error::other)?;
+
+        self.ws
+            .lock()
+            .await
+            .send(Message::Binary(request))
+            .await
+            .map_err(std::io::Error::other)?;
+
+        r.await.map_err(|_| std::io::Error::other("Closed"))?
+    }
+}
