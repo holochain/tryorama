410fb2460a879ac2de040fc29a823d5d8d8e2df9
Jost Schulte <jost.schulte@protonmail.com>
2024-09-30 17:06:13 -0600

    refactor(trycp): log error when removing player dir

--- crates/trycp_server/src/reset.rs
+++ crates/trycp_server/src/reset.rs
@@ -1,35 +1,18 @@
-use std::{io, sync::atomic};
+use std::sync::atomic;
 
 use nix::sys::signal::Signal;
-use snafu::{IntoError, Snafu};
 
 use crate::{
     app_interface, kill_player, ADMIN_PORT_RANGE, NEXT_ADMIN_PORT, PLAYERS, PLAYERS_DIR_PATH,
 };
 
-#[derive(Debug, Snafu)]
-pub(crate) enum ResetError {
-    #[snafu(display(
-        "Could not clear out players directory at {}: {}",
-        PLAYERS_DIR_PATH,
-        source
-    ))]
-    RemoveDir { source: io::Error },
-}
-
-pub(crate) fn reset() -> Result<(), ResetError> {
+pub(crate) fn reset() -> Result<(), ()> {
     let (players, app_connections, admin_connections) = {
         let mut players_guard = PLAYERS.write();
         let mut app_connections_guard =
             futures::executor::block_on(app_interface::APP_CONNECTIONS.lock());
         let mut admin_connections_guard =
             futures::executor::block_on(crate::admin_call::ADMIN_CONNECTIONS.lock());
-        NEXT_ADMIN_PORT.store(ADMIN_PORT_RANGE.start, atomic::Ordering::SeqCst);
-        match std::fs::remove_dir_all(PLAYERS_DIR_PATH) {
-            Ok(()) => {}
-            Err(e) if e.kind() == io::ErrorKind::NotFound => {}
-            Err(e) => return Err(RemoveDir.into_error(e)),
-        }
         (
             std::mem::take(&mut *players_guard),
             std::mem::take(&mut *app_connections_guard),
@@ -56,6 +39,11 @@ pub(crate) fn reset() -> Result<(), ResetError> {
         if let Err(e) = kill_player(player.processes.get_mut(), &id, Signal::SIGKILL) {
             println!("warn: failed to kill player {:?}: {}", id, e);
         }
+    }
+
+    NEXT_ADMIN_PORT.store(ADMIN_PORT_RANGE.start, atomic::Ordering::SeqCst);
+    if let Err(err) = std::fs::remove_dir_all(PLAYERS_DIR_PATH) {
+        println!("warn: could not remove directory {PLAYERS_DIR_PATH}: {err}");
     }
 
     Ok(())
