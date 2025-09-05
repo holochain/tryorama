73f6910c27f284bbecdd2f79d6cb236555e8919f
Jost Schulte <jost-s@users.noreply.github.com>
2025-09-05 19:36:06 +0200

    fix: fixture build script to work with upgraded getrandom

--- build-fixture.sh
+++ build-fixture.sh
@@ -4,10 +4,10 @@ set -e
 # build test hApp
 cd ts/test/fixture/zomes/integrity
 echo "build integrity zome..."
-cargo build --release --target wasm32-unknown-unknown
+RUSTFLAGS='--cfg getrandom_backend="custom"' cargo build --release --target wasm32-unknown-unknown
 cd ../coordinator
 echo "build coordinator zome..."
-cargo build --release --target wasm32-unknown-unknown
+RUSTFLAGS='--cfg getrandom_backend="custom"' cargo build --release --target wasm32-unknown-unknown
 cd ../.. # into fixtures
 hc dna pack . -o entry.dna
 hc app pack . -o entry.happ
