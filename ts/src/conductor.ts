4b2bb10637433297a1715a1f2a533ebc061b3c2c
Matt Gabrenya <matt@buildyourweb.app>
2025-09-30 06:46:47 -0700

    docs: note values other than 1 or 0 are rejected

--- ts/src/conductor.ts
+++ ts/src/conductor.ts
@@ -119,7 +119,9 @@ export interface NetworkConfig {
    *
    * This controls the range of DHT locations that the peer will aim to store and serve during gossip.
    *
-   * For leacher nodes that do not contribute to gossip, set to zero.
+   * For leacher nodes that do not contribute to gossip, set to 0.
+   * 
+   * Values other than 1 or 0 will be rejected.
    *
    * Default: 1
    */
