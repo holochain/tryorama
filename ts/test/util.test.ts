cc1f873d36c1532eb6cb5db6de0dcc774c390737
Matt Gabrenya <matt@buildyourweb.app>
2025-10-15 18:32:11 -0700

    test: remove test that can no longer be achieved, since integration_limbo empties too quickly, instead cover assertions in success case test

--- ts/test/util.test.ts
+++ ts/test/util.test.ts
@@ -78,8 +78,10 @@ test("dhtSync - Create multiple entries, read the last, 2 conductors", async () 
   await scenario.cleanUp();
 });
 
-test("dhtSync - Fails if some Ops are not integrated in a conductor", async () => {
-  const scenario = new Scenario();
+test("dhtSync - Fails if some Ops are not synced among all conductors", async () => {
+  const scenario = new Scenario({
+    disableLocalServices: true,
+  });
 
   const appBundleSource: AppBundleSource = {
     type: "path",
@@ -90,34 +92,19 @@ test("dhtSync - Fails if some Ops are not integrated in a conductor", async () =
     { appBundleSource },
   ]);
 
-  // Alice creates 1 entry, but never publishes it because publishing is disabled
+  // Alice creates 1 entry
   await alice.cells[0].callZome<EntryHash>({
     zome_name: TEST_ZOME_NAME,
     fn_name: "create",
     payload: "my entry",
   });
 
-  const x = true;
-  while (x) {
-    // Dump Bob's conductor state
-    const bobStateDump = await bob.conductor.adminWs().dumpFullState({
-      cell_id: alice.cells[0].cell_id,
-      dht_ops_cursor: undefined,
-    });
-
-    // When Bob recieves Alice's Ops, and they are being validated,
-    // then dhtSync should fail
-    if (bobStateDump.integration_dump.validation_limbo.length > 0) {
-      try {
-        // Run with a 0ms timeout, so that we check the sync status only *once*,
-        // while Bob's conductor is still in this state.
-        await dhtSync([alice], alice.cells[0].cell_id[0], 500, 0);
-        assert.fail();
-      } catch {
-        assert(true);
-      }
-      break;
-    }
+  // Bob does not receive the entry within the timeout
+  try {
+    await dhtSync([alice, bob], alice.cells[0].cell_id[0], undefined, 0);
+    assert.fail();
+  } catch {
+    assert(true);
   }
 
   await scenario.cleanUp();
