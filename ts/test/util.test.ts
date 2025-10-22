95f9b6062cf6c285689b2a88f7407534deb55a5a
Matt Gabrenya <matt@buildyourweb.app>
2025-10-22 10:20:50 -0700

    test: ensure scenarios are cleaned up on failure

--- ts/test/util.test.ts
+++ ts/test/util.test.ts
@@ -12,76 +12,73 @@ import { EMPTY_ARC, FULL_ARC } from "./constants";
 
 const TEST_ZOME_NAME = "coordinator";
 
-test("dhtSync - Create multiple entries, read the last, 2 conductors", async () => {
-  const scenario = new Scenario();
+test("dhtSync - Create multiple entries, read the last, 2 conductors", async () =>
+  runScenario(async (scenario: Scenario) => {
+    const appBundleSource: AppBundleSource = {
+      type: "path",
+      value: FIXTURE_HAPP_URL.pathname,
+    };
+    const [alice, bob] = await scenario.addPlayersWithApps([
+      { appBundleSource },
+      { appBundleSource },
+    ]);
 
-  const appBundleSource: AppBundleSource = {
-    type: "path",
-    value: FIXTURE_HAPP_URL.pathname,
-  };
-  const [alice, bob] = await scenario.addPlayersWithApps([
-    { appBundleSource },
-    { appBundleSource },
-  ]);
-
-  // Alice and Bob init their cells to trigger creation of InitZomesComplete Ops
-  await bob.cells[0].callZome<string>({
-    zome_name: TEST_ZOME_NAME,
-    fn_name: "init",
-    payload: null,
-  });
-  await alice.cells[0].callZome<string>({
-    zome_name: TEST_ZOME_NAME,
-    fn_name: "init",
-    payload: null,
-  });
-
-  // Alice creates 10 entries
-  let lastCreatedHash;
-  let lastCreatedContent;
-  for (let i = 0; i < 10; i++) {
-    lastCreatedContent = `Hi dare ${i}`;
-    lastCreatedHash = await alice.cells[0].callZome<EntryHash>({
+    // Alice and Bob init their cells to trigger creation of InitZomesComplete Ops
+    await bob.cells[0].callZome<string>({
       zome_name: TEST_ZOME_NAME,
-      fn_name: "create",
-      payload: lastCreatedContent,
+      fn_name: "init",
+      payload: null,
     });
-  }
+    await alice.cells[0].callZome<string>({
+      zome_name: TEST_ZOME_NAME,
+      fn_name: "init",
+      payload: null,
+    });
 
-  await dhtSync([alice, bob], alice.cells[0].cell_id[0]);
+    // Alice creates 10 entries
+    let lastCreatedHash;
+    let lastCreatedContent;
+    for (let i = 0; i < 10; i++) {
+      lastCreatedContent = `Hi dare ${i}`;
+      lastCreatedHash = await alice.cells[0].callZome<EntryHash>({
+        zome_name: TEST_ZOME_NAME,
+        fn_name: "create",
+        payload: lastCreatedContent,
+      });
+    }
 
-  // Bob gets the last created entry
-  const readContent = await bob.cells[0].callZome<string>({
-    zome_name: TEST_ZOME_NAME,
-    fn_name: "read",
-    payload: lastCreatedHash,
-  });
-  assert.equal(readContent, lastCreatedContent);
+    await dhtSync([alice, bob], alice.cells[0].cell_id[0]);
 
-  // Alice has no ops in validation or integration limbo
-  const aliceDump = await alice.conductor.adminWs().dumpFullState({
-    cell_id: alice.cells[0].cell_id,
-    dht_ops_cursor: undefined,
-  });
-  assert.equal(aliceDump.integration_dump.integration_limbo.length, 0);
-  assert.equal(aliceDump.integration_dump.validation_limbo.length, 0);
+    // Bob gets the last created entry
+    const readContent = await bob.cells[0].callZome<string>({
+      zome_name: TEST_ZOME_NAME,
+      fn_name: "read",
+      payload: lastCreatedHash,
+    });
+    assert.equal(readContent, lastCreatedContent);
 
-  // Bob has no ops in validation or integration limbo
-  const bobDump = await bob.conductor.adminWs().dumpFullState({
-    cell_id: bob.cells[0].cell_id,
-    dht_ops_cursor: undefined,
-  });
-  assert.equal(bobDump.integration_dump.integration_limbo.length, 0);
-  assert.equal(bobDump.integration_dump.validation_limbo.length, 0);
+    // Alice has no ops in validation or integration limbo
+    const aliceDump = await alice.conductor.adminWs().dumpFullState({
+      cell_id: alice.cells[0].cell_id,
+      dht_ops_cursor: undefined,
+    });
+    assert.equal(aliceDump.integration_dump.integration_limbo.length, 0);
+    assert.equal(aliceDump.integration_dump.validation_limbo.length, 0);
 
-  // Alice has 52 ops integrated
-  assert.equal(aliceDump.integration_dump.integrated.length, 52);
+    // Bob has no ops in validation or integration limbo
+    const bobDump = await bob.conductor.adminWs().dumpFullState({
+      cell_id: bob.cells[0].cell_id,
+      dht_ops_cursor: undefined,
+    });
+    assert.equal(bobDump.integration_dump.integration_limbo.length, 0);
+    assert.equal(bobDump.integration_dump.validation_limbo.length, 0);
 
-  // Bob has 52 ops integrated
-  assert.equal(bobDump.integration_dump.integrated.length, 52);
+    // Alice has 52 ops integrated
+    assert.equal(aliceDump.integration_dump.integrated.length, 52);
 
-  await scenario.cleanUp();
-});
+    // Bob has 52 ops integrated
+    assert.equal(bobDump.integration_dump.integrated.length, 52);
+  }));
 
 test("dhtSync - Fails if some Ops are not synced among all conductors", async () => {
   const scenario = new Scenario({
@@ -111,53 +108,48 @@ test("dhtSync - Fails if some Ops are not synced among all conductors", async ()
     assert.fail();
   } catch {
     assert(true);
+  } finally {
+    await scenario.cleanUp();
   }
-
-  await scenario.cleanUp();
 });
 
-test("storageArc - Succeeds for 2 conductors, both start as empty arc, one creates an entry, both reach full arc", async () => {
-  const scenario = new Scenario();
-  const appBundleSource: AppBundleSource = {
-    type: "path",
-    value: FIXTURE_HAPP_URL.pathname,
-  };
-  const [alice, bob] = await scenario.addPlayersWithApps([
-    { appBundleSource },
-    { appBundleSource },
-  ]);
+test("storageArc - Succeeds for 2 conductors, both start as empty arc, one creates an entry, both reach full arc", async () =>
+  runScenario(async (scenario: Scenario) => {
+    const appBundleSource: AppBundleSource = {
+      type: "path",
+      value: FIXTURE_HAPP_URL.pathname,
+    };
+    const [alice, bob] = await scenario.addPlayersWithApps([
+      { appBundleSource },
+      { appBundleSource },
+    ]);
 
-  // Alice & Bob have empty storage arcs
-  await Promise.all([
-    storageArc(alice, alice.cells[0].cell_id[0], EMPTY_ARC),
-    storageArc(bob, alice.cells[0].cell_id[0], EMPTY_ARC),
-  ]);
+    // Alice & Bob have empty storage arcs
+    await Promise.all([
+      storageArc(alice, alice.cells[0].cell_id[0], EMPTY_ARC),
+      storageArc(bob, alice.cells[0].cell_id[0], EMPTY_ARC),
+    ]);
 
-  // Alice and Bob have full storage arcs
-  await Promise.all([
-    storageArc(alice, alice.cells[0].cell_id[0], FULL_ARC),
-    storageArc(bob, alice.cells[0].cell_id[0], FULL_ARC),
-  ]);
+    // Alice and Bob have full storage arcs
+    await Promise.all([
+      storageArc(alice, alice.cells[0].cell_id[0], FULL_ARC),
+      storageArc(bob, alice.cells[0].cell_id[0], FULL_ARC),
+    ]);
+  }));
 
-  await scenario.cleanUp();
-});
+test("storageArc - Fails for only 1 conductor which never reaches full storage arc", async () =>
+  runScenario(async (scenario: Scenario) => {
+    const appBundleSource: AppBundleSource = {
+      type: "path",
+      value: FIXTURE_HAPP_URL.pathname,
+    };
+    const [alice] = await scenario.addPlayersWithApps([{ appBundleSource }]);
 
-test("storageArc - Fails for only 1 conductor which never reaches full storage arc", async () => {
-  const scenario = new Scenario();
-
-  const appBundleSource: AppBundleSource = {
-    type: "path",
-    value: FIXTURE_HAPP_URL.pathname,
-  };
-  const [alice] = await scenario.addPlayersWithApps([{ appBundleSource }]);
-
-  // Alice never reaches full storage arc
-  await expect(
-    storageArc(alice, alice.cells[0].cell_id[0], FULL_ARC),
-  ).rejects.toThrow();
-
-  await scenario.cleanUp();
-});
+    // Alice never reaches full storage arc
+    await expect(
+      storageArc(alice, alice.cells[0].cell_id[0], FULL_ARC),
+    ).rejects.toThrow();
+  }));
 
 test("integratedOpsCount - Succeeds when integrated Ops count matches", () =>
   runScenario(async (scenario: Scenario) => {
